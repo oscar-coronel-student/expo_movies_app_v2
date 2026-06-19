c93958d7220894813813ed9712f1b8a2857ad651
Oscar Coronel <oscar.coronel@conauto.com.ec>
2026-06-19 16:52:45 -0500

    Inicializar proyecto con Tailwind (NativeWind)

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+
+const RootLayout = () => {
+    return <>
+        <View className="flex-1 bg-red-600">
+            <Text className="">
+                Hola Mundo
+            </Text>
+        </View>
+    </>;
+}
+
+export default RootLayout;
